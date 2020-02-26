ea62c987d58f7a6178a991ace6ffb04481184717
Anderson <1501068760@mil>
2020-02-26 15:58:11 -0700

    4of13 at 8:57

---
+++ CraftSite/CraftSite.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using CraftSite.Website.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CraftSite.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment;
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
